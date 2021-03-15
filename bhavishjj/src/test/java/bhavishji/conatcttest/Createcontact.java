8fdde74854ff8cb6e575ae7dc02459c14a74f0d9
GABHI <GABHI@LAPTOP-V4H52QRD>
2021-03-15 01:03:44 -0700

    commit

---
+++ bhavishjj/src/test/java/bhavishji/conatcttest/Createcontact.java
@@ -0,0 +1,27 @@
+package bhavishji.conatcttest;
+
+import org.testng.annotations.Test;
+
+public class Createcontact
+{
+
+	@Test
+	public void createcontact()
+	{
+		System.out.println("create contact");
+	}
+	
+	@Test
+	public void modifycontact()
+	{
+		System.out.println("modify contact");
+	}
+
+	@Test
+	public void deletecontact()
+	{
+		System.out.println("delete contact");
+	}
+
+
+}
